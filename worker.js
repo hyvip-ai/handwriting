ce527f67a1add2480ece170683b6591ed997e377
hyvip-ai <rm2932002@gmail.com>
2021-07-08 22:36:24 +0530

    first

---
+++ worker.js
@@ -0,0 +1,10 @@
+self.importScripts('handwritten.js', 'blobstream.js')
+self.addEventListener('message', async (e) => {
+  const doc = await handwritten(e.data[0], {
+    ruled: e.data[1]
+  })
+  const stream = doc.pipe(blobStream())
+  stream.on('finish', () => {
+    self.postMessage(stream.toBlobURL('application/pdf'))
+  })
+})
